9e0ad679c75bb92d045c5688252e22589f32432b
Mikael Ribeiro <mikaelribeirors@gmail.com>
2023-02-03 20:11:43 -0300

    Estrutura de dados complexos

---
+++ script.js
@@ -0,0 +1,124 @@
+//01 - Crie um array que receba 5 itens e exiba no console.
+
+
+//let carros = ["Ford", "Jeep", "Gol", "Fiat", "Mustang"];
+//console.log(carros);
+
+
+//02 - Utilize um método para adicionar um nome ao inicio do array.
+
+
+//let carros = ["Ford", "Jeep", "Gol", "Fiat", "Mustang"];
+//carros.unshift("Tesla");
+//console.log(carros);
+
+
+//03 - Utilize um método para remover o último nome do array.
+
+
+//let carros = ["Ford", "Jeep", "Gol", "Fiat", "Mustang"];
+//carros.pop();
+//console.log(carros);
+
+
+//04 - Utilize um método para adicionar dois nomes ao fim do array.
+
+
+//let carros = ["Ford", "Jeep", "Gol", "Fiat", "Mustang"];
+//carros.push("Vasco","Gama");
+//console.log(carros);
+
+
+//05 - Utilize um método para remover o primeiro nome do array.
+
+
+//let carros = ["Ford", "Jeep", "Gol", "Fiat", "Mustang"];
+//carros.shift();
+//console.log(carros);
+
+
+//06 - Utilize um método para organizar em ordem crescente o seguinte array:
+
+
+//let numbers = [7,5,6,3,8,9,2,1,4]
+//numbers.sort();
+//console.log(numbers)
+
+
+//07 - Crie um objeto que receba ao menos três propriedades sobre você.
+
+
+// let objetos = {
+//    nomeSobrenome: "Mikael Ribeiro",
+//    idade: 20,
+//   time: "Vasco da Gama",
+//    comidaFavorita: "Strognofe de carne"
+//  }
+
+
+// console.log(objetos)
+
+
+//08 - Adicione uma nova propriedade sem alterar seu objeto inicial.
+//   objetos.corFavorita = "Branca"
+//console.log(objetos)
+
+
+//09 - Remova uma propriedade desse objeto.
+
+
+//delete objetos.idade 
+//console.log(objetos)
+
+
+//10 - Mostre no console todas as propriedades desse objeto.
+
+
+
+//11- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
+
+//let cadastro = [
+//    {
+//        nome: "Mikael",
+//        telefone: 3235-2124,
+//        idade:21,
+//        amigos:['Pedro', 'Bernardo' , 'Fabricio','Pietro']
+//    },
+//    {
+//        nome: "Lucas",
+//        telefone: 3235-2124,
+//        idade:24,
+//        amigos:["Paulo", "Fabio","joão","Mikael"]
+//    },
+//    {
+//        nome: "Pedro",
+//        telefone: 3235-2124,
+//        idade:20,
+//        amigos:["Diogo", "Eliseu","Gabriel","Mikael"]
+//    },
+//    {
+//        nome: "Arthur",
+//        telefone: 3235-2124,
+//        idade:7,
+//        amigos:["Pedro", "Arthur","Eduardo","Mikael"]
+//    },
+//    {
+//        nome: "Felipe",
+//        telefone: 3235-2124,
+//        idade:22,
+//       amigos:["Thiago", "Bruno","Fernado","Alexandre"]
+//    }
+//]
+
+
+//12 - Mostre no console o nome de um amigo de cada lista.
+
+
+//console.log(cadastro[0].amigos[0])
+//console.log(cadastro[1].amigos[1])
+//console.log(cadastro[2].amigos[2])
+//console.log(cadastro[3].amigos[3])
+//console.log(cadastro[4].amigos[1])
